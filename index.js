521b548de40ab9e2ca6cb18fb79d1f99dd6b4a98
Sandra <sandyjtech@gmail.com>
2023-04-16 14:56:50 -0700

    added index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById("main").remove();
+let newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+newHeader.innerHTML ="Sandra is the champion";
+newHeader.append();
